83be45a21d89108231012c4b5e2ce24531fb65f1
chun950yi <chun950yi@gmail.com>
2022-01-22 02:02:04 +0800

    project created

---
+++ StockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockApp
+//
+//  Created by 竣亦 on 2022/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
